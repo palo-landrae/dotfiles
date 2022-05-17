9e8b77a8731fbd34203e5f6fdcbaa1aad747086d
palo-landrae <louiandraepalo@gmail.com>
2022-05-17 23:07:53 +0000

    :sparkles: nvim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-log_file=~/install_progress_log.txt
-
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 grep -qxF 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' ~/.bashrc || echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' >> ~/.bashrc
 
@@ -13,3 +11,8 @@ echo 'alias commit="git add * && git commit -m "' >> ~/.bashrc
 # z
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
 echo . ~/z.sh >> ~/.bashrc
+
+# nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+g clone https://github.com/palo-landrae/dotfiles-nvim ~/.config/nvim

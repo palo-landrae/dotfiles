4eb8e54afad620a9728a91f4ef3a8327d520d9e1
palo-landrae <louiandraepalo@gmail.com>
2022-05-17 22:10:29 +0000

    :tada: setup - ohmyposh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+log_file=~/install_progress_log.txt
+
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' >> ~/.bashrc

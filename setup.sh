e0e23e3764b18ae5e3a7fe68c6ff440accf9b969
palo-landrae <louiandraepalo@gmail.com>
2022-05-17 22:30:44 +0000

    :sparkles: alias

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ brew install jandedobbeleer/oh-my-posh/oh-my-posh
 grep -qxF 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' ~/.bashrc || echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' >> ~/.bashrc
 
 brew install neovim
+
+# setting alias
+echo 'alias g="git"' >> ~/.bashrc
+echo 'alias v="nvim"' >> ~/.bashrc
+echo 'alias commit="git add * && git commit -m "' >> ~/.bashrc

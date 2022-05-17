9c057a5e97eff11e8186c209baa72e2ce3e4a0a9
palo-landrae <louiandraepalo@gmail.com>
2022-05-17 22:26:56 +0000

    :sparkles: neovim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 log_file=~/install_progress_log.txt
 
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
-echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' >> ~/.bashrc
+grep -qxF 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' ~/.bashrc || echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/takuya.omp.json')"' >> ~/.bashrc
+
+brew install neovim

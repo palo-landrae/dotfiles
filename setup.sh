1b0eea086273f5fe01af0525f9abb5b790f33030
Loui Andrae Palo <louiandraepalo@gmail.com>
2022-05-18 13:51:58 +0200

    :sparkles: added zsh

--- setup.sh
+++ setup.sh
@@ -16,3 +16,6 @@ echo . ~/z.sh >> ~/.bashrc
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/palo-landrae/dotfiles-nvim ~/.config/nvim
+
+# Angular
+npm install -g @angular/cli
---
+++ zsh/setup.sh
@@ -0,0 +1,17 @@
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# setting alias
+echo 'alias g="git"' >> ~/.zshrc
+echo 'alias v="nvim"' >> ~/.zshrc
+echo 'alias commit="git add * && git commit -m "' >> ~/.zshrc
+echo 'alias cls="clear"' >> ~/.zshrc
+
+# z
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
+echo . ~/z.sh >> ~/.zshrc
+
+# nvim
+brew install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone https://github.com/palo-landrae/dotfiles-nvim ~/.config/nvim

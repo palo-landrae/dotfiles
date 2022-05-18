e85099e5e6c02737511395d0a7df3c9f54f119bc
Loui Andrae Palo <louiandraepalo@gmail.com>
2022-05-18 18:58:32 +0200

    :sparkles: removed silent cmd

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -17,6 +17,6 @@ brew install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/palo-landrae/dotfiles-nvim ~/.config/nvim
-nvim -es -i NONE -c "PlugInstall" -c "qa"
+nvim -i NONE -c "PlugInstall" -c "qa"
 
 exec zsh -l
